a00a577b958cc0d99742ec1743120d76659aece6
Ilya Taziev <tazievii@gmail.com>
2020-07-24 15:57:51 +0500

    Выбор типа

--- src/components/PizzaTypes.js
+++ src/components/PizzaTypes.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 function PizzaTypes({ items, onClick }) {
+  const [state, setstate] = React.useState(null);
+  const selectItem = (index) => {
+    setstate(index);
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
+        <li className={state === null ? 'active' : ''} onClick={() => selectItem(null)}>Все</li>
         {items.map((item, index) => (
           <li 
-          onClick={() => onClick(item)}
-          key={`${item}_${index}`}>{item}</li>
+          className={state === index ? 'active' : ''}
+          onClick={() => selectItem(index)} 
+          key={`${item}_${index}`}>
+          {item}
+          </li>
         ))}
       </ul>
     </div>
