ed3193a7e03e711b4063c5d2f05e15b6824ea66f
Ilya Taziev <tazievii@gmail.com>
2020-07-26 12:58:48 +0500

    верстка пиццы в компонент

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import { Header } from './components';
 import { HomePage, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      })
+      .catch((err) => console.log(err.message('Ошибка')));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/cart" component={Cart} />
+        <Route exact path="/" render={() => <HomePage items={pizzas} />} />
+        <Route exact path="/cart" component={Cart} />
       </div>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import PizzaTypes from './PizzaTypes';
 import Sort from './Sort';
+import PizzaItem from './PizzaItem';
 
-export { Header, PizzaTypes, Sort };
+export { Header, PizzaTypes, Sort, PizzaItem };
