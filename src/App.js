57ef74855ac1c6ac0203a28292e488d1e294165f
Ilya Taziev <tazievii@gmail.com>
2020-07-26 23:09:25 +0500

    асинхронный запрос пицц

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { Route } from 'react-router-dom';
 import { Header } from './components';
 import { HomePage, Cart } from './pages';
-import { setPizzas } from './redux/actions/pizzas';
-
+import { fetchPizzas } from './redux/actions/pizzas';
 
 function App() {
   const dispatch = useDispatch();
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    dispatch(fetchPizzas());
   }, [dispatch]);
 
   return (
--- src/components/PizzaTypes.js
+++ src/components/PizzaTypes.js
@@ -5,25 +5,26 @@ const PizzaTypes = React.memo(({ items, onClick }) => {
   const selectItem = (index) => {
     setstate(index);
     onClick(index);
-  }
+  };
 
   return (
     <div className="categories">
       <ul>
-        <li className={state === null ? 'active' : ''} onClick={() => selectItem(null)}>Все</li>
-        {items && items.map((item, index) => (
-          <li 
-          className={state === index ? 'active' : ''}
-          onClick={() => selectItem(index)} 
-          key={`${item}_${index}`}>
-          {item}
-          </li>
-        ))}
+        <li className={state === null ? 'active' : ''} onClick={() => selectItem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((item, index) => (
+            <li
+              className={state === index ? 'active' : ''}
+              onClick={() => selectItem(index)}
+              key={`${item}_${index}`}>
+              {item}
+            </li>
+          ))}
       </ul>
     </div>
   );
-})
-
-
+});
 
 export default PizzaTypes;
